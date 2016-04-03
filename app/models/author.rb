08144258acafa00212c9d3cd9d8238eb4c04d924
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2016-04-03 16:00:38 +0100

    Removed the "models" and "managers" from the app and put that in the gem.

--- app/models/author.rb
+++
@@ -1,6 +0,0 @@
-class Author
-  include Mongoid::Document
-  field :name
-  field :_id, default: -> { name }
-  has_many :articles
-end
--- app/models/comment.rb
+++
@@ -1,7 +0,0 @@
-class Comment
-  include Mongoid::Document
-  field :name
-  field :content
-
-  embedded_in :article, inverse_of: :comments
-end
